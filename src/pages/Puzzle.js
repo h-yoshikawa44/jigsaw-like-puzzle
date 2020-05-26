48dfba1ed120f844d3819d52ca6bfa5685cf9bd0
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2020-05-26 15:40:30 +0900

    パズル画面と当サービスについて画面をpagesへ移動 #19

--- src/pages/Puzzle.js
+++ src/pages/Puzzle.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PuzzleTemplate from './templates/PuzzleTemplate';
+import PuzzleTemplate from '../templates/PuzzleTemplate';
 
 const Puzzle = () => {
   const imageUrl = `${process.env.PUBLIC_URL}/photo0000_6339.png`;
