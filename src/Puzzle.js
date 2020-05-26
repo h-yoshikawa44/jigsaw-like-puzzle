2467d0be881938ac93bf05a799fc3b6ea5eb926d
h-yoshikawa0724 <guardian.yatagaras@icloud.com>
2020-05-26 13:04:36 +0900

    Canvas部分のコンポーネント分割 #19

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Divider } from '@material-ui/core';
 import Konva from 'konva';
-import { Stage, Layer, Group, Rect, Line, Image } from 'react-konva';
 import useImage from 'use-image';
 import _shuffle from 'lodash/shuffle';
 import PuzzleGuide from './organisms/PuzzleGuide';
 import SelectDifficultyModal from './organisms/SelectDifficultyModal';
 import CompleteModal from './organisms/CompleteModal';
 import PauseModal from './organisms/PauseModal';
+import PuzzleCanvas from './organisms/PuzzleCanvas';
 
 const Puzzle = () => {
   const [hour, setHour] = useState('00');
@@ -92,7 +92,7 @@ const Puzzle = () => {
     }
   }, [matchPieceCount]);
 
-  const handleSelectDifficultyAction = async (diffculty) => {
+  const handleSelectDifficultyAction = (diffculty) => {
     switch (diffculty) {
       case 'easy':
         setPieceXCount(6);
@@ -179,15 +179,6 @@ const Puzzle = () => {
   };
 
   const [image] = useImage(`${process.env.PUBLIC_URL}/photo0000_6339.png`);
-  const flameWidth = 40;
-  const imageWidth = 720;
-  const imageHeight = 480;
-  const padding = 50;
-  /* global window */
-  const stageWidth = window.innerWidth;
-  const stageHeight = imageHeight + flameWidth * 2 + padding * 2;
-  const imageFlameX = stageWidth / 2 - (imageWidth + flameWidth * 2) / 2;
-  const initialPieceSpaceX = -180;
 
   return (
     <div>
@@ -200,179 +191,12 @@ const Puzzle = () => {
         handlePauseAction={handlePauseAction}
       />
       <Divider />
-      <Stage width={stageWidth} height={stageHeight}>
-        <Layer>
-          <Group x={imageFlameX} y={padding}>
-            <Rect
-              width={imageWidth}
-              height={imageHeight}
-              x={flameWidth}
-              y={flameWidth}
-              fill="white"
-            />
-            <Image
-              image={image}
-              opacity={0.6}
-              width={imageWidth}
-              height={imageHeight}
-              x={flameWidth}
-              y={flameWidth}
-            />
-            <Line
-              points={[
-                0,
-                0,
-                flameWidth * 2 + imageWidth,
-                0,
-                flameWidth + imageWidth,
-                flameWidth,
-                flameWidth,
-                flameWidth,
-              ]}
-              closed
-              fillLinearGradientStartPoint={{
-                x: flameWidth + imageWidth / 2,
-                y: 0,
-              }}
-              fillLinearGradientEndPoint={{
-                x: flameWidth + imageWidth / 2,
-                y: flameWidth,
-              }}
-              fillLinearGradientColorStops={[0, '#b34e06', 1, '#501300']}
-            />
-            <Line
-              points={[
-                0,
-                0,
-                flameWidth,
-                flameWidth,
-                flameWidth,
-                flameWidth + imageHeight,
-                0,
-                flameWidth * 2 + imageHeight,
-              ]}
-              closed
-              fillLinearGradientStartPoint={{
-                x: 0,
-                y: flameWidth + imageHeight / 2,
-              }}
-              fillLinearGradientEndPoint={{
-                x: flameWidth,
-                y: flameWidth + imageHeight / 2,
-              }}
-              fillLinearGradientColorStops={[0, '#b34e06', 1, '#501300']}
-            />
-            <Line
-              points={[
-                flameWidth,
-                flameWidth + imageHeight,
-                flameWidth + imageWidth,
-                flameWidth + imageHeight,
-                flameWidth * 2 + imageWidth,
-                flameWidth * 2 + imageHeight,
-                0,
-                flameWidth * 2 + imageHeight,
-              ]}
-              closed
-              fillLinearGradientStartPoint={{
-                x: flameWidth + imageWidth / 2,
-                y: flameWidth * 2 + imageHeight,
-              }}
-              fillLinearGradientEndPoint={{
-                x: flameWidth + imageWidth / 2,
-                y: flameWidth + imageHeight,
-              }}
-              fillLinearGradientColorStops={[0, '#b34e06', 1, '#501300']}
-            />
-            <Line
-              points={[
-                flameWidth + imageWidth,
-                flameWidth,
-                flameWidth * 2 + imageWidth,
-                0,
-                flameWidth * 2 + imageWidth,
-                flameWidth * 2 + imageHeight,
-                flameWidth + imageWidth,
-                flameWidth + imageHeight,
-              ]}
-              closed
-              fillLinearGradientStartPoint={{
-                x: flameWidth * 2 + imageWidth,
-                y: flameWidth + imageHeight / 2,
-              }}
-              fillLinearGradientEndPoint={{
-                x: flameWidth + imageWidth,
-                y: flameWidth + imageHeight / 2,
-              }}
-              fillLinearGradientColorStops={[0, '#b34e06', 1, '#501300']}
-            />
-            <Line
-              points={[
-                0,
-                0,
-                flameWidth,
-                flameWidth,
-                flameWidth,
-                flameWidth + imageHeight,
-                0,
-                flameWidth * 2 + imageHeight,
-              ]}
-              closed
-              fillLinearGradientStartPoint={{
-                x: 0,
-                y: flameWidth + imageHeight / 2,
-              }}
-              fillLinearGradientEndPoint={{
-                x: flameWidth,
-                y: flameWidth + imageHeight / 2,
-              }}
-              fillLinearGradientColorStops={[0, '#b34e06', 1, '#501300']}
-            />
-            <Line
-              points={[
-                0,
-                0,
-                flameWidth * 2 + imageWidth,
-                0,
-                flameWidth + imageWidth,
-                flameWidth,
-                flameWidth,
-                flameWidth,
-              ]}
-              closed
-              fillLinearGradientStartPoint={{
-                x: flameWidth + imageWidth / 2,
-                y: 0,
-              }}
-              fillLinearGradientEndPoint={{
-                x: flameWidth + imageWidth / 2,
-                y: flameWidth,
-              }}
-              fillLinearGradientColorStops={[0, '#b34e06', 1, '#501300']}
-            />
-          </Group>
-        </Layer>
-        <Layer>
-          <Group x={imageFlameX + flameWidth} y={padding + flameWidth}>
-            {shuffledPieceInfo.map((piece) => {
-              return (
-                <Image
-                  key={piece.id}
-                  id={piece.id}
-                  image={image}
-                  crop={piece.crop}
-                  width={piece.width}
-                  height={piece.height}
-                  x={initialPieceSpaceX}
-                  draggable
-                  onDragStart={handleDragStart}
-                  onDragEnd={handleDragEnd}
-                />
-              );
-            })}
-          </Group>
-        </Layer>
-      </Stage>
+      <PuzzleCanvas
+        image={image}
+        shuffledPieceInfo={shuffledPieceInfo}
+        handleDragStart={handleDragStart}
+        handleDragEnd={handleDragEnd}
+      />
       <SelectDifficultyModal
         open={difficultyModalOpen}
         handleSelectDifficultyAction={handleSelectDifficultyAction}
