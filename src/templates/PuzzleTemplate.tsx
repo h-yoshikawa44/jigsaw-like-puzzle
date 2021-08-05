1335466b44b7b6f31b4d83936094bc85300a61c4
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-08-05 16:29:01 +0900

    refactor: パズルロジックをカスタムフックに分離(#55)

--- src/templates/PuzzleTemplate.tsx
+++ src/templates/PuzzleTemplate.tsx
@@ -1,16 +1,14 @@
 import { VFC, useState, useEffect } from 'react';
 import { Divider } from '@material-ui/core';
-import Konva from 'konva';
-import _shuffle from 'lodash/shuffle';
 import useImage from 'use-image';
 import PuzzleGuide from 'components/organisms/PuzzleGuide';
 import SelectDifficultyModal from 'components/organisms/SelectDifficultyModal';
 import CompleteModal from 'components/organisms/CompleteModal';
 import PauseModal from 'components/organisms/PauseModal';
 import PuzzleCanvas from 'components/organisms/PuzzleCanvas';
-import useCounter from 'hooks/useCounter';
 import { Diffculty } from 'models/Diffculty';
-import { Piece } from 'models/Piece';
+import useCounter from 'hooks/useCounter';
+import usePuzzle from 'hooks/usePuzzle';
 
 type Props = {
   imageUrl: string;
@@ -18,77 +16,37 @@ type Props = {
 
 const PuzzleTemplate: VFC<Props> = ({ imageUrl }) => {
   const { time, countStart, countStop, countReset } = useCounter();
+  const {
+    pieceData,
+    pieceTotalCount,
+    matchPieceCount,
+    selectDifficulty,
+    handlePieceDragStart,
+    handlePieceDragEnd,
+    pieceCountReset,
+  } = usePuzzle();
 
   const [difficultyModalOpen, setDifficultyModalOpen] = useState(true);
   const [pauseModalOpen, setPauseModalOpen] = useState(false);
   const [completeModalOpen, setCompleteModalOpen] = useState(false);
-  const [matchPieceCount, setMatchPieceCount] = useState(0);
-  const [pieceXCount, setPieceXCount] = useState(0);
-  const [pieceYCount, setPieceYCount] = useState(0);
-  const [pieceSize, setPieceSize] = useState(0);
-  const [shuffledPieceInfo, setShuffledPieceInfo] = useState<Piece[]>([]);
 
   const handlePauseAction = () => {
     countStop();
     setPauseModalOpen(true);
   };
-  useEffect(() => {
-    const initialPiece = () => {
-      const pieceInfo = [];
-      const randomKey = Math.random().toString(32).substring(2);
-      for (let y = 0; y < pieceYCount; y += 1) {
-        for (let x = 0; x < pieceXCount; x += 1) {
-          pieceInfo.push({
-            id: `${y}-${x}-${randomKey}`,
-            crop: {
-              x: pieceSize * x,
-              y: pieceSize * y,
-              width: pieceSize,
-              height: pieceSize,
-            },
-            width: pieceSize,
-            height: pieceSize,
-          });
-        }
-      }
-      setShuffledPieceInfo(_shuffle(pieceInfo));
-    };
-    if (pieceXCount && pieceYCount && pieceSize) {
-      initialPiece();
-    }
-  }, [pieceXCount, pieceYCount, pieceSize]);
 
   useEffect(() => {
     const complete = () => {
       setCompleteModalOpen(true);
     };
-    if (matchPieceCount && matchPieceCount === pieceXCount * pieceYCount) {
+    if (matchPieceCount && matchPieceCount === pieceTotalCount) {
       countStop();
       setTimeout(complete, 1500);
     }
-  }, [matchPieceCount, pieceXCount, pieceYCount, countStop]);
+  }, [pieceTotalCount, matchPieceCount, countStop]);
 
   const handleSelectDifficultyAction = (diffculty: Diffculty) => {
-    switch (diffculty) {
-      case 'easy':
-        setPieceXCount(6);
-        setPieceYCount(4);
-        setPieceSize(120);
-        break;
-      case 'normal':
-        setPieceXCount(9);
-        setPieceYCount(6);
-        setPieceSize(80);
-        break;
-      case 'hard':
-        setPieceXCount(12);
-        setPieceYCount(8);
-        setPieceSize(60);
-        break;
-      default:
-        // eslint-disable-next-line no-console
-        console.log('error');
-    }
+    selectDifficulty(diffculty);
     setDifficultyModalOpen(false);
     countStart();
   };
@@ -99,60 +57,8 @@ const PuzzleTemplate: VFC<Props> = ({ imageUrl }) => {
   const handleRestartAction = () => {
     setCompleteModalOpen(false);
     countReset();
-    setPieceXCount(0);
-    setPieceYCount(0);
-    setMatchPieceCount(0);
+    pieceCountReset();
     setDifficultyModalOpen(true);
-  };
-  const checkNumber = (pieceNum: number, fixedPosition: number) => {
-    if (pieceNum > fixedPosition + 20 || pieceNum < fixedPosition - 20) {
-      return false;
-    }
-    return true;
-  };
-  const handleDragStart = (e: Konva.KonvaEventObject<DragEvent>) => {
-    e.target.setAttrs({
-      shadowOffset: {
-        x: 15,
-        y: 15,
-      },
-      scaleX: 1.1,
-      scaleY: 1.1,
-      shadowColor: 'gray',
-      shadowBlur: 10,
-      shadowOpacity: 0.6,
-    });
-    const { id } = e.target.attrs;
-    const pieces = shuffledPieceInfo.slice();
-    const piece = pieces.find((i) => i.id === id);
-    if (piece) {
-      const index = pieces.indexOf(piece);
-      pieces.splice(index, 1);
-      pieces.push(piece);
-    }
-    setShuffledPieceInfo(pieces);
-  };
-  const handleDragEnd = (e: Konva.KonvaEventObject<DragEvent>) => {
-    const update = {
-      duration: 0.5,
-      easing: Konva.Easings.ElasticEaseOut,
-      scaleX: 1,
-      scaleY: 1,
-      shadowOffsetX: 0,
-      shadowOffsetY: 0,
-    };
-    if (
-      checkNumber(e.target.attrs.x, e.target.attrs.cropX) &&
-      checkNumber(e.target.attrs.y, e.target.attrs.cropY)
-    ) {
-      setMatchPieceCount((count) => count + 1);
-      Object.assign(update, {
-        draggable: false,
-        x: e.target.attrs.cropX,
-        y: e.target.attrs.cropY,
-      });
-    }
-    e.target.to(update);
   };
 
   const [image, status] = useImage(imageUrl);
@@ -161,7 +67,7 @@ const PuzzleTemplate: VFC<Props> = ({ imageUrl }) => {
     <div>
       <PuzzleGuide
         matchPieceCount={matchPieceCount}
-        pieceTotalCount={pieceXCount * pieceYCount}
+        pieceTotalCount={pieceTotalCount}
         hour={time.hour}
         minutes={time.minutes}
         seconds={time.seconds}
@@ -171,9 +77,9 @@ const PuzzleTemplate: VFC<Props> = ({ imageUrl }) => {
       {status === 'loaded' && (
         <PuzzleCanvas
           image={image}
-          shuffledPieceInfo={shuffledPieceInfo}
-          handleDragStart={handleDragStart}
-          handleDragEnd={handleDragEnd}
+          shuffledPieceInfo={pieceData}
+          handleDragStart={handlePieceDragStart}
+          handleDragEnd={handlePieceDragEnd}
         />
       )}
       <SelectDifficultyModal
