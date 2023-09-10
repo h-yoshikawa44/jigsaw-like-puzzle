6163ce682db1d6820dbc6df74a695adf3c28dce2
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2023-09-10 18:36:23 +0900

    feat: lodashからjustへ移行(#88)

--- src/hooks/usePuzzle.ts
+++ src/hooks/usePuzzle.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback } from 'react';
 import Konva from 'konva';
-import _shuffle from 'lodash/shuffle';
+import shuffle from 'just-shuffle';
 import { EASY, NORMAL, HARD } from 'constants/puzzle';
 import { Diffculty } from 'models/Diffculty';
 import { Piece } from 'models/Piece';
@@ -47,7 +47,7 @@ const usePuzzle = (): {
           });
         }
       }
-      setPieceData(_shuffle(pieceInfo));
+      setPieceData(shuffle(pieceInfo));
     };
     if (pieceCount.x && pieceCount.y && pieceSize) {
       initialPiece();
