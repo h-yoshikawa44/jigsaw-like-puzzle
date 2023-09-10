e28bf6f09a7abeadbfe0ed9dbd712e0be8d3f8af
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2023-09-10 19:49:10 +0900

    fix: パスが間違っていたので修正(#89)
    Viteではindex.htmlはルート階層にある必要がある。

--- src/containers/pages/Puzzle.tsx
+++ src/containers/pages/Puzzle.tsx
@@ -4,7 +4,7 @@ import Puzzle from 'components/pages/Puzzle';
 import { Diffculty } from 'models/Diffculty';
 import useCounter from 'hooks/useCounter';
 import usePuzzle from 'hooks/usePuzzle';
-import puzzleImg from './assets/photo0000_6339.png';
+import puzzleImg from 'assets/photo0000_6339.png';
 
 const EnhancedPuzzle: FC = () => {
   const [image, status] = useImage(puzzleImg);
